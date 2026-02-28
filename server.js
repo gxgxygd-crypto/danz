c08d5a284bf99addc9b2b4459583ad7acad64f62
gxgxygd-crypto <gxgxygd@gmail.com>
2026-02-28 14:11:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -314,7 +314,7 @@ wss.on("connection", (ws) => {
 // =============================================
 // START SERVER
 // =============================================
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`
 ╔════════════════════════════════════════╗
